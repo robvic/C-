3de2f94c661b2767f649b15493531d263ea1e447
Roberto Victor <robertovictor_rj@hotmail.com>
2020-07-21 03:42:54 -0300

    Initial commit

---
+++ Audio Fix/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Audio_Fix
+{
+    partial class frmIDSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnProbe = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(314, 32);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 32);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(279, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(279, 282);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnProbe
+            // 
+            this.btnProbe.Location = new System.Drawing.Point(314, 69);
+            this.btnProbe.Name = "btnProbe";
+            this.btnProbe.Size = new System.Drawing.Size(75, 23);
+            this.btnProbe.TabIndex = 3;
+            this.btnProbe.Text = "Probe";
+            this.btnProbe.UseVisualStyleBackColor = true;
+            this.btnProbe.Click += new System.EventHandler(this.BtnProbe_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(314, 107);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // frmIDSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 374);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnProbe);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "frmIDSearch";
+            this.Text = "ID Search";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnProbe;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Audio Fix/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Audio_Fix
+{
+    public partial class frmIDSearch : Form
+    {
+        public frmIDSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            string strPath = txtPath.Text;
+            int counter = 0;
+
+            //Regex
+            string pattern = @"IdExibicao=""(\w+)""";
+            
+
+
+            string target = "IdExibicao=";
+            int iFrom;
+            string line, firstPart, secondPart;
+            List<string> result = new List<string>();
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"D:\OneDrive - Globo Comunicação e Participações sa\03.Projetos\Globo - Audio Fix\getRedeIds\A) EXIBICAO - GATEWAY - RIO HD_2020-01-12_00-00-00.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                //string input = line;
+                //MatchCollection collection = Regex.Matches(input, pattern);
+                //foreach (Match m in collection)
+                //{
+                //    result.Add(m.Value);
+                //}
+                if (line.IndexOf(target) > 1)
+                {
+                    iFrom = line.IndexOf("IdExibicao=");
+                    firstPart = line.Substring(iFrom + target.Length).Substring(1);
+                    secondPart = firstPart.Substring(0, firstPart.IndexOf("\""));
+                    result.Add(secondPart);
+                }
+                counter++;
+            }
+
+            file.Close();
+
+            List<string> uniqueList = result.Distinct().ToList();
+
+            uniqueList.ForEach(i => Console.WriteLine($"{i}"));
+            System.Console.WriteLine("Count of ID: {0}", result.Count);
+            System.Console.WriteLine("Count of ID: {0}", uniqueList.Count);
+            System.Console.WriteLine("There were {0} lines.", counter);
+
+            var source = new BindingSource
+            {
+                DataSource = uniqueList
+            };
+            dataGridView1.DataSource = source;
+
+            //var list = new BindingList<string>(uniqueList);
+            //dataGridView1.DataSource = list.;
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            string output = $"output-{DateTime.Now.Ticks}.mp4";
+            string input = "sample.mxf";
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "ffmpeg.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = $"-i {input} {output}";
+            using (Process exeProcess = Process.Start(startInfo))
+            {
+                exeProcess.WaitForExit();
+            }
+            string argument = "/select, \"" + output + "\"";
+            Process.Start("explorer.exe", argument);
+        }
+
+        private void BtnProbe_Click(object sender, EventArgs e)
+        {
+            string input = "sample.mxf";
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "CMD.EXE";
+            startInfo.Arguments = $"/k ffprobe.exe -i {input} -show_streams -select_streams a:0";
+            Process p = Process.Start(startInfo);
+            p.WaitForExit();
+     
+        }
+    }
+}
